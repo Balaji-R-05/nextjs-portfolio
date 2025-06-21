de3ac0a221e4679998ae8610e5ac4054716eee85
Balaji-R-05 <balajiramu23@gmail.com>
2025-06-22 01:51:11 +0530

    Added skill svgs and completed Hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,57 @@
+import { FaLocationArrow } from "react-icons/fa6";
+import { cn } from "@/lib/utils";
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div
+        className={cn(
+          "absolute top-0 left-0 w-full h-full",
+          "[background-size:100px_100px]",
+          "[background-image:linear-gradient(to_right,rgba(255,255,255,0.1)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.03)_1px,transparent_1px)]"
+        )}
+      />
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-[#000319] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            AI • Cloud • Full-Stack • Innovation
+          </p>
+          <TextGenerateEffect
+            words="Building Smart, Human-Centered Tech for the Future"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Balaji, an aspiring AI/ML Engineer - from Chennai, India.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Explore my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ data/index.ts
@@ -0,0 +1,31 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#project" },
+  { name: "Experience", link: "#experience" },
+  { name: "Testimonials", link: "#testimonial" },
+  { name: "Contact", link: "#contact" },
+];
+
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/github.svg",
+    link: "https://github.com/Balaji-R-05"
+  },
+  {
+    id: 2,
+    img: "/twitter.svg",
+    link: ""
+  },
+  {
+    id: 3,
+    img: "/linkedin.svg",
+    link: "https://www.linkedin.com/in/balaji-ramu05/"
+  },
+  {
+    id: 4,
+    img: "/instagram.svg",
+    link: ""
+  }
+];
